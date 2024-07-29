3c5735a3aa1f461c3a3f5654470abaf81774ab23
mbynae <piowm123@gmail.com>
2024-07-29 10:57:26 +0900

    노션 캐시 제거 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     //메타데이터Url은 반드시 배포용 Url로 적용
-    metadataBase: new URL('https://nextjs-study-fpo2.vercel.app/'),
+    metadataBase: new URL('https://nextjs-study-fpo2.vercel.app?nocache=1'),
     title: 'Next.js 연습용 프로젝트',
     description: 'Next.js 공식문서 스터디 및 연습용 개인 프로젝트입니다.',
     icons: {
