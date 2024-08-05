c593bb2219d0ee1ee5f4fbcb3dbc8fde52c7537d
mbynae <piowm123@gmail.com>
2024-08-05 23:02:14 +0900

    casl introduce 챕터 스터디

--- src/func/access/ability.ts
+++ src/func/access/ability.ts
@@ -11,16 +11,18 @@ interface User {
 //     cannot('delete', 'User');
 // });
 
-// const ability = (user: any) =>
-//     defineAbility((can) => {
-//         can('read', 'Article');
+const ability = (user: any) =>
+    defineAbility((can, cannot) => {
+        can('read', 'Article');
+        cannot('read', 'Article', { private: true });
 
-//         if (user.isLoggedIn) {
-//             can('update', 'Article', { authorId: user.id });
-//             can('create', 'Comment');
-//             can('update', 'Comment', { authorId: user.id });
-//         }
-//     });
+        if (user.isLoggedIn) {
+            can('read', 'Article', { authorId: user.id, private: true });
+            can('update', 'Article', { authorId: user.id });
+            can('create', 'Comment');
+            can('update', 'Comment', { authorId: user.id });
+        }
+    });
 
 // const ability = (user: { id: number; isLoggedIn: boolean }) =>
 //     defineAbility((can) => {
@@ -28,15 +30,21 @@ interface User {
 //         can('read', 'Article', { published: false, sharedWith: user.id });
 //     });
 
-const ability = (user: User) =>
-    defineAbility((can) => {
-        can('read', 'Article');
-        can('update', 'Article', ['title', 'description'], { authorId: user.id });
+// const ability = (user: User) =>
+//     defineAbility((can) => {
+//         can('read', 'Article');
+//         can('update', 'Article', ['title', 'description'], { authorId: user.id });
 
-        if (user.admin) {
-            can('update', 'Article', ['published']);
-        }
-    });
+//         if (user.admin) {
+//             can('update', 'Article', ['published']);
+//         }
+//     });
+
+// const ability = (user: User) =>
+//     defineAbility((can, cannot) => {
+//         cannot('read', 'all', { private: true }).because('비공개 게시물은 열람할 수 없습니다.');
+//         can('read', 'all', { authorId: user.id });
+//     });
 
 export default ability;
 
@@ -66,6 +74,26 @@ export default ability;
 //어떤 액션에 대해 특정 행동만 컨트롤 하도록 하는 인자 (배열로 적용)
 //can에서 사용할 땐 string으로 각각 개별적으로 판별해야됨
 
+//4. 반전 규칙 cannot
+//cannot은 definedAbility의 콜백함수 2번째 매개변수로 금지해야할 경우를 설정하기 위해 사용
+//can이 병렬적으로 배치 시 or 연산이 되지만 can과 cannot은 and 연산으로 진행됨
+//can과 cannot은 후자가 덮어쓰기 때문에 순서가 중요하다
+//cannot.because("금지 이유"): ForbiddenError에서 금지 이유를 반환받을 수 있다.
+
+//5. 업데이트
+//ability.update(): ability 인스턴스를 업데이트하는 기능. update 매개변수에 값을 넣음
+//[]: 모든 내용 금지, [action: "read", subject: "all"]: 읽기 전용 모드로 전환
+//createMongoAbility, AbilityBuilder를 사용해 ability 규칙 생성 가능
+//const ability = createMongoAbility(): 어빌리티 인스턴스 생성
+//const {can, rule} = new AbilityBuilder(createMongoAbility): 규칙 지정 함수
+//ability.update(rule): 어빌리티 인스턴스 업데이트
+//pureAbility를 활용해 업데이트 이전, 이후를 추적 가능
+//const unsubscribe = ability.on("update", ({rules, target}) => {
+////rules: update 함수에 전달된 배열
+////target: 이벤트를 트리거한 pureAbility 인스턴스
+//})
+//unsubscribe(): 구독 제거
+
 //시나리오
 //유저리스트 출력 (1번만 admin:true)
 //로그인 => 로그인 후 계정 정보 (id(name), 고유번호, isLoggedIn)
@@ -73,3 +101,8 @@ export default ability;
 //게시물 수정/삭제: 작성자만
 //게시물 공유 (sharedWith) => 작성자의 모든 글 등록한 유저가 보기 가능
 //admin일 시 모든 글 보기 / 삭제 가능
+//cannot을 통해 로그인이 되어있지 않으면 글쓰기 금지
+//because + ForbiddenError를 통해 글쓰기를 하려면 로그인 하라는 멘트 출력
+//로그아웃 시 비공개 글만 볼 수 있도록 ability update
+//로그인 후 본인 프로필 클릭 시 post 페이지로 이동하도록 ability에 access 이벤트 추가 (AbilityBuilder 사용)
+//리다이렉트는 ability.on을 사용하고 언마운트 시 구독 제거 하도록 useEffect 사용 (custom Hooks로 ability.on 함수 제작)
--- src/func/access/example.ts
+++ src/func/access/example.ts
@@ -1,13 +1,42 @@
+import { AbilityBuilder, createMongoAbility, ForbiddenError } from '@casl/ability';
 import defineAbilityFor from './ability';
 import { Article, Comment } from './entities';
 
-// 읽기 가능, 수정: 아이디 일치, 댓글쓰기: 로그인만, 댓글수정: 아이디 일치
-// const user = { id: 1, isLoggedIn: true };
-// const ability = defineAbilityFor(user);
-// const ownArticle = new Article({ authorId: user.id });
-// const anotherArticle = new Article({ authorId: 2 });
-// const ownComment = new Comment({ authorId: user.id });
-// const anotherComment = new Comment({ authorId: 2 });
+// 읽기: private가 false경우 전부 읽기, true일 경우 id가 일치할때만, 수정: 아이디 일치, 댓글쓰기: 로그인만, 댓글수정: 아이디 일치
+// update: 모든 행동이 배열로 들어간 인스턴스만 행동 가능하게 업데이트됨 (빈 배열일 시 모든 행동 불가, ex) 로그아웃일 때 읽기만 가능하도록 설정)
+// 새 어빌리티 생성, 규칙 생성 및 추가
+// ability.on 함수로 업데이트 전("update"), 업데이트 후("updated") 규칙 배열과 이벤트 타겟 확인 및 추가 이벤트 실행 가능
+const user = { id: 1, isLoggedIn: true };
+const ability = defineAbilityFor(user);
+const ownArticle = new Article({ authorId: user.id, private: false });
+const privateArticle = new Article({ authorId: user.id, private: true });
+const anotherArticle = new Article({ authorId: 2, private: true });
+const ownComment = new Comment({ authorId: user.id });
+const anotherComment = new Comment({ authorId: 2 });
+
+const unsubscribe = ability.on('update', ({ rules, target }) => {
+    console.log(rules, target);
+});
+
+ability.update([
+    { action: 'read', subject: 'Article', conditions: { private: false } },
+    { action: 'read', subject: 'Comment' },
+]);
+ability.update([...ability.rules, { action: 'delete', subject: 'Article', conditions: { authorId: user.id } }]); //이렇게 규칙 추가 가능
+
+const ability2 = createMongoAbility(); //어빌리티 인스턴스 생성
+const { can, rules } = new AbilityBuilder(createMongoAbility); //규칙 생성
+can('read', 'all');
+
+ability2.update(rules); //규칙 추가
+can('update', 'Article');
+ability2.update(rules.concat({ action: 'access', subject: 'Setting', conditions: { isLoggedIn: user.isLoggedIn } })); //규칙 추가
+
+unsubscribe();
+
+// console.log(ability2.can('read', anotherArticle));
+
+// console.log(ability.can('update', ownArticle));
 
 // 모두 공개, 비공개 + 공유자만 가능, 비공개 or 비공유자
 // const user = { id: 1, isLoggedIn: true };
@@ -16,11 +45,23 @@ import { Article, Comment } from './entities';
 // const article2 = new Article({ published: false, sharedWith: user.id });
 // const article3 = new Article({ published: false, sharedWith: 2 });
 
-// 읽기: 모두 가능, 제목/내용 수정:아이디 일치만, 등록: 관리자만
-const admin = { id: 1, isLoggedIn: true, admin: true };
-const user = { id: 2, isLoggedIn: true, admin: false };
-const ability = defineAbilityFor(user);
-const ownArticle = new Article({ authorId: user.id });
-const foreignArticle = new Article({ authorId: 3 });
+// // 읽기: 모두 가능, 제목/내용 수정:아이디 일치만, 등록: 관리자만
+// const admin = { id: 1, isLoggedIn: true, admin: true };
+// const user = { id: 2, isLoggedIn: true, admin: false };
+// const ability = defineAbilityFor(user);
+// const ownArticle = new Article({ authorId: user.id });
+// const foreignArticle = new Article({ authorId: 3 });
 
-console.log(ability.can('update', ownArticle, 'published'));
+// 읽기: id가 일치할 시 가능, private가 true면 불가, 불가 시 에러 메세지 출력
+// const user = { id: 1, isLoggedIn: true, admin: false };
+// const ability = defineAbilityFor(user);
+
+// try {
+//     ForbiddenError.from(ability).throwUnlessCan('read', { private: true, authorId: user.id });
+// } catch (error) {
+//     if (error instanceof ForbiddenError) {
+//         console.log(error.message); // 비공개 게시물은 열람할 수 없습니다.
+//     }
+
+//     throw error;
+// }
