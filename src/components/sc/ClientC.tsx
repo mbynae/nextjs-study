0da1bc3edefdb31ce2ef9905918d843ef1d9391e
mbynae <piowm123@gmail.com>
2024-08-28 00:23:50 +0900

    use server 학습

--- src/components/sc/ClientC.tsx
+++ src/components/sc/ClientC.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useActionState } from 'react';
-import { serverActionTodoUser } from '@/func/serverAction/serverActionExample';
+import { useActionState, useState, useTransition } from 'react';
+import { increaseServerAction, serverActionTodoUser } from '@/func/serverAction/serverActionExample';
 
 import TextInput from '../common/textInput/TextInput';
 import SquareBtn from '../common/button/SquareBtn';
@@ -16,12 +16,32 @@ type Props = {
 const ClientC = ({ onClick }: Props) => {
     const [state, action, ispending] = useActionState(serverActionTodoUser, null);
 
+    const [count, setCount] = useState(0);
+    const [isPending, startTransition] = useTransition();
+
+    const increaseAction = async () => {
+        startTransition(async () => {
+            const result = await increaseServerAction(count);
+            setCount(result);
+        });
+    };
+
+    console.log(state);
     return (
         <form action={action} className={styles.form}>
-            <TextInput name="userId" disabled={ispending} />
-            <SquareBtn className={styles.button} disabled={ispending}>
-                제출
-            </SquareBtn>
+            {!state && <span className={styles.defaultState}>값을 입력해주세요.</span>}
+            <div className={styles.group}>
+                <TextInput name="userId" disabled={ispending} defaultValue={count} />
+                <SquareBtn className={styles.button} disabled={ispending}>
+                    제출
+                </SquareBtn>
+            </div>
+            <div className={styles.inceeaseGroup}>
+                <span>초기번호: {count}</span>
+                <SquareBtn formAction={increaseAction} disabled={isPending}>
+                    증가 버튼
+                </SquareBtn>
+            </div>
         </form>
     );
 };
--- src/func/serverAction/serverActionExample.ts
+++ src/func/serverAction/serverActionExample.ts
@@ -6,6 +6,12 @@ import { TODOLIST_TYPES } from '@/api/type/todoType';
 export async function serverActionTodoUser(init: TODOLIST_TYPES.UsetData[] | null, formData: FormData) {
     const id = formData.get('userId') as string;
 
+    if (!id) return null;
+
     const result = await fetchTodoUserList(id);
     return result;
+}
+
+export async function increaseServerAction(count: number) {
+    return count + 1;
 }
